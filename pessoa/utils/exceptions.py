96cf24214e5e04300fce6d6b2fb1cba997f54395
RichardMatheus929 <richardmatheus929@gmail.com>
2024-08-09 14:14:01 -0300

    change file path modules

--- pessoa/utils/exceptions.py
+++ pessoa/utils/exceptions.py
@@ -6,6 +6,11 @@ class NullData(APIException):
     default_code = 'data not could be null'
 
 class PessoaNotExist(APIException):
-    status_code = 404
+    status_code = 400
     default_detail = 'A pessoa não pode ser editada porque não existe'
     default_code = 'people not exists'
+
+class InvalidData(APIException):
+    status_code = 400
+    default_detail = "Os dados inseridos são inválidos"
+    default_code = 'invalid data'
